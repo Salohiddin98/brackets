be182804a0d7e682ff6eebf16340c447f4c90bee
Salohiddin98 <salohiddin.rashidov98@gmail.com>
2022-02-26 23:39:33 +0500

    feat:add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  str = Array.from(str);
+  if (str.length % 2 != 0) {
+    return false;
+  } else {
+    str.forEach((bracket) => {
+      bracketsConfig.forEach((el) => {
+        if (bracket === el[0] && stack[stack.length - 1] != el[1]) {
+          stack.push(bracket);
+        } else if (bracket === el[1]) {
+          if (stack.length === 0) {
+            return false;
+          } else {
+            if (stack[stack.length - 1] === el[0]) {
+              stack.pop();
+            } else {
+              return false;
+            }
+          }
+        }
+      });
+    });
+  }
+  if (stack.length != 0) {
+    return false;
+  }
+  return true;
+};
